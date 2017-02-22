49f10449934e5e0f50e5402049730bd21b594beb
yuzumone <yuzumone@outlook.com>
2017-02-22 13:48:58 +0900

    Update coupon info list fragment

--- app/src/main/kotlin/net/yuzumone/bergamio/fragment/CouponInfoListFragment.kt
+++ app/src/main/kotlin/net/yuzumone/bergamio/fragment/CouponInfoListFragment.kt
@@ -14,9 +14,11 @@ import net.yuzumone.bergamio.databinding.FragmentCouponInfoListBinding
 import net.yuzumone.bergamio.databinding.ItemCouponInfoBinding
 import net.yuzumone.bergamio.model.CouponInfo
 import net.yuzumone.bergamio.model.HdoInfo
+import net.yuzumone.bergamio.model.PacketLogInfo
 import net.yuzumone.bergamio.util.PreferenceUtil
 import net.yuzumone.bergamio.view.ArrayRecyclerAdapter
 import net.yuzumone.bergamio.view.BindingHolder
+import net.yuzumone.bergamio.view.RecyclerItemClickListener
 import rx.Subscription
 import rx.android.schedulers.AndroidSchedulers
 import rx.schedulers.Schedulers
@@ -26,12 +28,18 @@ import javax.inject.Inject
 
 class CouponInfoListFragment : BaseFragment() {
 
-    private val ARG_COUPON_INFO = "coupon_info"
     private lateinit var binding: FragmentCouponInfoListBinding
     private lateinit var couponInfo: ArrayList<CouponInfo>
+    private lateinit var packetLogs: ArrayList<PacketLogInfo>
     private lateinit var adapter: CouponInfoAdapter
     @Inject lateinit var client: MioponClient
     @Inject lateinit var compositeSubscription: CompositeSubscription
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        couponInfo = ArrayList<CouponInfo>()
+        packetLogs = ArrayList<PacketLogInfo>()
+    }
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = FragmentCouponInfoListBinding.inflate(inflater, container, false)
@@ -41,15 +49,31 @@ class CouponInfoListFragment : BaseFragment() {
     }
 
     private fun initView() {
+        activity.title = activity.getString(R.string.app_name)
+        binding.swipeRefresh.isEnabled = false
+        binding.swipeRefresh.setColorSchemeResources(R.color.colorPrimary)
         adapter = CouponInfoAdapter(activity)
         binding.recyclerView.adapter = adapter
         binding.recyclerView.layoutManager = LinearLayoutManager(activity)
+        binding.recyclerView.addOnItemTouchListener(
+                RecyclerItemClickListener(activity, object: RecyclerItemClickListener.OnItemClickListener {
+                    override fun onItemClick(view: View, position: Int) {
+                        val info = adapter.getItem(position)
+                        val log = packetLogs[position]
+                        val fragment = ViewPagerFragment.newInstance(info, log)
+                        fragmentManager.beginTransaction().replace(R.id.content, fragment)
+                                .addToBackStack(null).commit()
+                    }
+                })
+        )
     }
 
     private fun fetchCoupon(developer: String, token: String): Subscription {
         return client.getCoupon(developer, token)
                 .subscribeOn(Schedulers.newThread())
                 .observeOn(AndroidSchedulers.mainThread())
+                .doOnSubscribe { binding.swipeRefresh.isRefreshing = true }
+                .doOnCompleted { binding.swipeRefresh.isRefreshing = false }
                 .subscribe (
                         { response ->
                             couponInfo = ArrayList<CouponInfo>(response.couponInfo)
@@ -61,26 +85,35 @@ class CouponInfoListFragment : BaseFragment() {
                 )
     }
 
+    private fun fetchLog(developer: String, token: String): Subscription {
+        return client.getLog(developer, token)
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe (
+                        { response ->
+                            packetLogs = ArrayList<PacketLogInfo>(response.packetLogInfo)
+                        },
+                        { error ->
+                            Toast.makeText(activity, error.message, Toast.LENGTH_SHORT).show()
+                        }
+                )
+    }
+
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        if (savedInstanceState == null) {
+        if (couponInfo.size == 0 && packetLogs.size == 0) {
             val dev = BuildConfig.DEVELOPER_ID
             val token = PreferenceUtil.loadToken(activity)
             compositeSubscription.add(fetchCoupon(dev, token))
+            compositeSubscription.add(fetchLog(dev, token))
         } else {
-            couponInfo = savedInstanceState.getParcelableArrayList(ARG_COUPON_INFO)
             adapter.addAllWithNotify(couponInfo)
         }
     }
 
-    override fun onSaveInstanceState(outState: Bundle?) {
-        super.onSaveInstanceState(outState)
-        outState!!.putParcelableArrayList(ARG_COUPON_INFO, couponInfo)
-    }
-
-    override fun onDestroy() {
+    override fun onDestroyView() {
         compositeSubscription.unsubscribe()
-        super.onDestroy()
+        super.onDestroyView()
     }
 
     class CouponInfoAdapter(context: Context) : ArrayRecyclerAdapter<CouponInfo,
