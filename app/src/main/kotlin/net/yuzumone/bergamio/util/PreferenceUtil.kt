4a39e8829eb5d36999bb8048cf6bd3f7cec21b9e
yuzumone <yuzumone@outlook.com>
2017-11-05 18:48:41 +0900

    Use property
    close #23

--- app/src/main/kotlin/net/yuzumone/bergamio/util/PreferenceUtil.kt
+++ app/src/main/kotlin/net/yuzumone/bergamio/util/PreferenceUtil.kt
@@ -1,52 +1,32 @@
 package net.yuzumone.bergamio.util
 
 import android.content.Context
-import android.preference.PreferenceManager
 
-class PreferenceUtil {
-    companion object {
-        private val TOKEN = "token"
-        private val EXPIRE = "expire"
-        fun loadToken(context: Context): String {
-            val preferences = PreferenceManager.getDefaultSharedPreferences(context)
-            val token  = preferences.getString(TOKEN, null)
-            if (token == null) {
-                return ""
-            } else {
-                return token
-            }
+class PreferenceUtil(context: Context) {
+
+    private val filename = "net.yuzumone.bergamio.pref"
+    private val preference = context.getSharedPreferences(filename, 0)
+    private val TOKEN = "token"
+    private val EXPIRE = "expire"
+
+    var token: String
+    get() = preference.getString(TOKEN, null)
+    set(value) = preference.edit().putString(TOKEN, value).apply()
+
+    val hasAvailableToken: Boolean
+    get() {
+        if (token == "" && expire == 0L) {
+            return false
         }
-        fun storeToken(context: Context, token: String) {
-            val preferences = PreferenceManager.getDefaultSharedPreferences(context)
-            val editor = preferences.edit()
-            editor.putString(TOKEN, token)
-            editor.apply()
-        }
-        fun loadExpire(context: Context): Long {
-            val preferences = PreferenceManager.getDefaultSharedPreferences(context)
-            val expire  = preferences.getLong(EXPIRE, 0L)
-            if (expire == 0L) {
-                return 0L
-            } else {
-                return expire
-            }
-        }
-        fun storeExpire(context: Context, expire: String) {
-            val now = System.currentTimeMillis() / 1000L
-            val expireTime = now + expire.toLong()
-            val preferences = PreferenceManager.getDefaultSharedPreferences(context)
-            val editor = preferences.edit()
-            editor.putLong(EXPIRE, expireTime)
-            editor.apply()
-        }
-        fun hasAvailableToken(context: Context): Boolean {
-            val token = loadToken(context)
-            val expire = loadExpire(context)
-            if (token == "" && expire == 0L) {
-                return false
-            }
-            val now = System.currentTimeMillis() / 1000L
-            return (now < expire) && (token != "")
-        }
+        val now = System.currentTimeMillis() / 1000L
+        return (now < expire) && (token != "")
+    }
+
+    var expire: Long
+    get() = preference.getLong(EXPIRE, 0L)
+    set(value) {
+        val now = System.currentTimeMillis() / 1000L
+        val tile = now + value
+        preference.edit().putLong(EXPIRE, tile).apply()
     }
 }
